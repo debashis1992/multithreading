18348a283fa8bed4d339786c1b905acb1c1fdb3f
debashis1992 <d2011das@gmail.com>
2020-08-21 23:49:50 +0530

    adding logging fix

--- twitter-clone-backend/src/main/java/com/clone/twitter/config/LoggingUtils.java
+++ twitter-clone-backend/src/main/java/com/clone/twitter/config/LoggingUtils.java
@@ -1,10 +1,13 @@
 package com.clone.twitter.config;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import org.jboss.logging.Logger;
 import org.springframework.http.HttpRequest;
@@ -14,54 +17,50 @@ import org.springframework.web.util.WebUtils;
 
 @Component
 public class LoggingUtils {
-	
+
 	public static final Logger logger = Logger.getLogger(LoggingUtils.class);
-	
+
 	public void traceRequest(HttpRequest httpRequest, byte[] body) throws IOException {
 		String httpMethod = httpRequest.getMethodValue();
 		String url = httpRequest.getURI().toString();
 		String httpHeaders = httpRequest.getHeaders().toSingleValueMap().toString();
 		String requestBody = body.toString();
-		
+
 		trace(httpMethod, url, httpHeaders, requestBody, "INCOMING");
 	}
-	
+
 	public void traceResponse(HttpRequest httpRequest, byte[] body) throws IOException {
 		String httpMethod = httpRequest.getMethodValue();
 		String url = httpRequest.getURI().toString();
 		String httpHeaders = httpRequest.getHeaders().toSingleValueMap().toString();
 		String requestBody = body.toString();
-		
+
 		trace(httpMethod, url, httpHeaders, requestBody, "OUTGOING");
 	}
 
 	public void preHandle(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String httpMethod = request.getMethod();
 		String url = request.getRequestURL().toString();
-		
+
 		Enumeration<String> headersEnum = request.getHeaderNames();
 		StringBuilder sbHeaders = new StringBuilder();
-		while(headersEnum.hasMoreElements()) {
+		while (headersEnum.hasMoreElements()) {
 			String headerKey = headersEnum.nextElement();
 			String headerVal = request.getHeader(headerKey);
 			sbHeaders.append(headerKey).append(" : ").append(headerVal).append(" ");
 		}
 		String requestBody = null;
-		if("POST".equals(httpMethod) || "PUT".equals(httpMethod)) {
+		if ("POST".equals(httpMethod) || "PUT".equals(httpMethod)) {
 			requestBody = request.getInputStream().readAllBytes().toString();
 		}
-		
+
 		trace(httpMethod, url, sbHeaders.toString(), requestBody, "INCOMING");
 	}
-	
+
 	public void postHandle(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
-//		byte[] body = responseWrapper.getContentAsByteArray();
-//		String responseBody = new String(body);
-//		responseWrapper.copyBodyToResponse();
-//		logger.info("Outgoing response : "+responseBody);
+		
 	}
-	
+
 	private void trace(String httpMethod, String url, String httpHeaders, String requestBody, String requestType) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n");
